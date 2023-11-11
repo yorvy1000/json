ae3e7835aa44b1cce1b410ee9be2f0dff78e7d5c
yorvy1000 <51828948+yorvy1000@users.noreply.github.com>
2023-11-11 12:44:44 -0400

    Create code.js

---
+++ code.js
@@ -0,0 +1,170 @@
+[
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9626",
+"hora_sorteo": "12:45:00",
+"sorteo": "TRIPLE ZULIA A",
+"numero_ganador": "52"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9626",
+"hora_sorteo": "12:45:00",
+"sorteo": "TRIPLE ZULIA B",
+"numero_ganador": "69"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9626",
+"hora_sorteo": "12:45:00",
+"sorteo": "TRIPLE ZULIA C",
+"numero_ganador": "02"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9626",
+"hora_sorteo": "12:45:00",
+"sorteo": "TRIPLE ZULIA A",
+"numero_ganador": "652"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9626",
+"hora_sorteo": "12:45:00",
+"sorteo": "TRIPLE ZULIA B",
+"numero_ganador": "869"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9626",
+"hora_sorteo": "12:45:00",
+"sorteo": "TRIPLE ZULIA C",
+"numero_ganador": "402"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9626",
+"hora_sorteo": "12:45:00",
+"sorteo": "ZODIACO DEL ZULIA",
+"numero_ganador": "02 ESC"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9626",
+"hora_sorteo": "12:45:00",
+"sorteo": "ZODIACO DEL ZULIA",
+"numero_ganador": "402 ESC"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9627",
+"hora_sorteo": "16:45:00",
+"sorteo": "TRIPLE ZULIA A",
+"numero_ganador": "01"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9627",
+"hora_sorteo": "16:45:00",
+"sorteo": "TRIPLE ZULIA B",
+"numero_ganador": "48"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9627",
+"hora_sorteo": "16:45:00",
+"sorteo": "TRIPLE ZULIA C",
+"numero_ganador": "16"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9627",
+"hora_sorteo": "16:45:00",
+"sorteo": "TRIPLE ZULIA A",
+"numero_ganador": "301"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9627",
+"hora_sorteo": "16:45:00",
+"sorteo": "TRIPLE ZULIA B",
+"numero_ganador": "848"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9627",
+"hora_sorteo": "16:45:00",
+"sorteo": "TRIPLE ZULIA C",
+"numero_ganador": "416"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9627",
+"hora_sorteo": "16:45:00",
+"sorteo": "ZODIACO DEL ZULIA",
+"numero_ganador": "16 TAU"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9627",
+"hora_sorteo": "16:45:00",
+"sorteo": "ZODIACO DEL ZULIA",
+"numero_ganador": "416 TAU"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9628",
+"hora_sorteo": "19:05:00",
+"sorteo": "TRIPLE ZULIA A",
+"numero_ganador": "93"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9628",
+"hora_sorteo": "19:05:00",
+"sorteo": "TRIPLE ZULIA B",
+"numero_ganador": "06"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9628",
+"hora_sorteo": "19:05:00",
+"sorteo": "TRIPLE ZULIA C",
+"numero_ganador": "72"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9628",
+"hora_sorteo": "19:05:00",
+"sorteo": "TRIPLE ZULIA A",
+"numero_ganador": "693"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9628",
+"hora_sorteo": "19:05:00",
+"sorteo": "TRIPLE ZULIA B",
+"numero_ganador": "206"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9628",
+"hora_sorteo": "19:05:00",
+"sorteo": "TRIPLE ZULIA C",
+"numero_ganador": "072"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9628",
+"hora_sorteo": "19:05:00",
+"sorteo": "ZODIACO DEL ZULIA",
+"numero_ganador": "72 LIB"
+},
+{
+"fecha_sorteo": "2023-11-10",
+"nro_sorteo": "9628",
+"hora_sorteo": "19:05:00",
+"sorteo": "ZODIACO DEL ZULIA",
+"numero_ganador": "072 LIB"
+}
+]
